9bfb3027379263a486829489a0b091902b0a883f
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-28 20:15:26 +0900

    fix(InputView): 구입 금액이 0인 경우 예외처리

--- src/main/java/lotto/application/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/application/validator/PurchaseAmountValidator.java
@@ -8,7 +8,7 @@ public class PurchaseAmountValidator {
         if (purchaseAmount % LOTTO_PRICE > 0) {
             throw new IllegalArgumentException("[ERROR] 잘못된 구입 금액 단위입니다.");
         }
-        if (purchaseAmount < 0) {
+        if (purchaseAmount < 1) {
             throw new IllegalArgumentException("[ERROR] 0보다 큰 양수 값이 아닙니다.");
         }
     }
