fdb7164b497a2ec78e204316e5bde03cbf32b2c9
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-28 20:54:43 +0900

    feat(LottoController): 로또 서비스 전체 흐름 제어 컨트롤러 구현

---
+++ src/main/java/lotto/presentation/LottoController.java
@@ -0,0 +1,52 @@
+package lotto.presentation;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.application.validator.LottoValidator;
+import lotto.application.validator.PurchaseAmountValidator;
+import lotto.common.NumberValidator;
+import lotto.domain.Lotto;
+import lotto.ui.InputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+
+    public LottoController() {
+        this.inputView = new InputView();
+    }
+
+    public int purchaseAmount() {
+        while (true) {
+            try {
+                String input = inputView.purchaseAmount();
+                NumberValidator.validateNumber(input);
+                int purchaseAmount = Integer.parseInt(input);
+
+                PurchaseAmountValidator.validatePurchaseAmount(purchaseAmount);
+                return purchaseAmount;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public Lotto drawNumbers() {
+        while (true) {
+            try {
+                String input = inputView.drawNumbers();
+                List<String> tokens = Arrays.stream(input.split(","))
+                        .map(String::trim)
+                        .toList();
+                NumberValidator.validateNumbers(tokens);
+
+                List<Integer> numbers = tokens.stream()
+                        .map(Integer::parseInt).toList();
+                LottoValidator.validateRanges(numbers);
+                return new Lotto(numbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
