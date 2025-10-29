c0c8e5bc63f107515deccaab58c5a619763f9ba1
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-29 10:24:49 +0900

    feat(LottoTicket): 로또 티켓 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,20 +1,28 @@
 package lotto;
 
+import java.util.List;
 import lotto.domain.Bonus;
 import lotto.domain.Lotto;
+import lotto.domain.LottoTicket;
 import lotto.presentation.LottoController;
+import lotto.ui.InputView;
+import lotto.ui.OutputView;
 
 public class Application {
-
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        LottoController controller = new LottoController();
+        InputView inputView = new InputView();
+        LottoController controller = new LottoController(inputView);
         int purchaseAmount = controller.purchaseAmount();
+        int purchaseCount = controller.purchaseCount(purchaseAmount);
+
+        OutputView outputView = new OutputView();
+        outputView.purchaseCount(purchaseCount);
+
+        List<LottoTicket> tickets = controller.drawTickets(purchaseCount);
+        outputView.lottoTickets(tickets);
+
         Lotto lotto = controller.drawNumbers();
         Bonus bonus = controller.drawBonus(lotto);
+
     }
-
-
-
-
 }
