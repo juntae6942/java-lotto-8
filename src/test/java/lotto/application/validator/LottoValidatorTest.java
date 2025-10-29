7075bf9e7278da0ae56d3c6a307fbc3cb94f00e6
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-29 16:08:10 +0900

    test(LottoValidator): 로또 번호, 보너스 번호 유효 범위 테스트

--- src/test/java/lotto/application/validator/LottoValidatorTest.java
+++ src/test/java/lotto/application/validator/LottoValidatorTest.java
@@ -21,4 +21,18 @@ class LottoValidatorTest {
         assertThatThrownBy(() -> LottoValidator.validateRange(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호에 범위를 벗어난 값이 들어오면 예외가 발생한다.")
+    @Test
+    void numberRangeTest() {
+        assertThatThrownBy(() -> LottoValidator.validateRanges(List.of(50, 3, 4, 10, 1, 2)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호에 범위를 벗어난 값이 들어오면 예외가 발생한다.")
+    @Test
+    void bonusNumberRangeTest() {
+        assertThatThrownBy(() -> LottoValidator.validateRange(50))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
