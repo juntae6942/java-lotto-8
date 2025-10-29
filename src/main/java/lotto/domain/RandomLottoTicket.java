a79ca8ba5e20ab06af669c0c49f75dd7c8ecd7cb
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-29 22:14:55 +0900

    refactor(LottoTicket): 로또 번호 범위 상수 지정

--- src/main/java/lotto/domain/RandomLottoTicket.java
+++ src/main/java/lotto/domain/RandomLottoTicket.java
@@ -6,10 +6,13 @@ import java.util.stream.Collectors;
 
 public class RandomLottoTicket implements LottoTicket {
 
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int NUMBERS_PER_TICKET = 6;
     private final List<Integer> numbers;
 
     public RandomLottoTicket() {
-        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        this.numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, NUMBERS_PER_TICKET);
     }
 
     @Override
