84a69c9246679e82b11a65ceef0be10efd773e0e
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-29 14:17:53 +0900

    feat(ProfitRateCalculator): 수익률 계산기 구현

---
+++ src/main/java/lotto/domain/ProfitRateCalculator.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.Map;
+
+public class ProfitRateCalculator {
+
+    private final Map<Rank, Integer> prizeByRank = Map.of(
+            Rank.FIRST, 2000000000,
+            Rank.SECOND, 30000000,
+            Rank.THIRD, 1500000,
+            Rank.FOURTH, 50000,
+            Rank.FIFTH, 5000,
+            Rank.NONE, 0
+    );
+
+    public double calculateProfitRate(int purchaseAmount, Map<Rank, Integer> winningStatistics) {
+        double totalPrizes = 0;
+        for (Rank rank : winningStatistics.keySet()) {
+            int prize = prizeByRank.get(rank);
+            int count = winningStatistics.get(rank);
+            totalPrizes += prize * count;
+        }
+        return Math.round(totalPrizes / purchaseAmount * 10) / 10.0;
+    }
+}
